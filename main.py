af1d73e1043b5783a116554b1f35a0d4b765fb04
Jonathan Ogwumike <49166200+Jogwums@users.noreply.github.com>
2025-04-15 23:00:46 +0100

    Add files via upload
    Updated files

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st 
 import pandas as pd 
+import plotly.express as px
 import altair as alt 
 
+@st.cache_data
 def load_data():
     df = pd.read_csv("chocosales.csv")
     # convert Date col to datetime datatype
@@ -15,6 +17,8 @@ df = load_data()
 
 # App title
 st.title("Chocolate Sales App")
+# Data table preview
+# st.dataframe(df.head(10))
 
 # create filters 
 filters = {
@@ -40,28 +44,52 @@ for key, selected_values in selected_filters.items():
                                   isin(selected_values)]
 
 # display the data
-st.dataframe(filtered_df.head())
+# st.dataframe(filtered_df.head())
 
 # calculations 
 no_of_transactions = len(filtered_df)
 total_revenue = filtered_df["Amount"].sum()
 total_boxes = filtered_df["Boxes Shipped"].sum()
 no_of_products = filtered_df["Product"].nunique()
+pert_of_boxes = f"{(total_boxes/df["Boxes Shipped"].sum())*100:,.2f}%"
 
-# streamlit column component 
+# st.write("%_boxes_for_selection:",pert_of_boxes)
+
+# streamlit column component
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     st.metric("Transactions", no_of_transactions)
 
 with col2:
-    st.metric("Total Revenue", total_revenue)
+    st.metric("Total Revenue", f"${total_revenue:,.0f}")
 
 with col3: 
-    st.metric("Total boxes", total_boxes)
+    st.metric("Total boxes", f"{total_boxes:,.0f}", delta="")
 
 with col4:
     st.metric("Products", no_of_products)
+
+st.subheader("Sales Trends")
+
+# mini-visuals 
+# to be displayed in columns
+col5, col6 = st.columns(2)
+
+
+with col5:
+    monthly_revenue = filtered_df.groupby(filtered_df['Date'].dt.month_name())['Amount'].sum().reset_index()
+    fig = px.line(monthly_revenue, x='Date', y='Amount', title="Monthly Revenue Trend")
+    st.plotly_chart(fig)
+
+with col6:
+    qtr = filtered_df["Date"].dt.quarter.unique()
+    qtrly_revenue = filtered_df.groupby(filtered_df["Date"].dt.quarter)["Amount"].sum().reset_index()
+    # st.write(qtrly_revenue)
+    fig = px.line(qtrly_revenue, x='Date', y='Amount', title="Qtrly Revenue Trend",
+                  labels=["Qtr","Sales"])
+    st.plotly_chart(fig)
+
 
 # charts 
 st.subheader("Products with largest revenue")
@@ -84,3 +112,28 @@ chart1 = alt.Chart(top_products).mark_bar().encode(
 
 # display the chart
 st.altair_chart(chart1, use_container_width=True)
+
+# Q3 sales by country
+# Group by country and sum the revenue
+sales_by_country = filtered_df.groupby('Country')['Amount'].sum().sort_values(ascending=False)
+
+# chart 3
+col7, col8 = st.columns(2)
+sales_by_country = filtered_df.groupby('Country')['Amount'].sum().reset_index()
+sales_by_country.columns = ["Country","Amount"]
+# st.write(sales_by_country)
+
+with col7:
+    st.subheader("Sales by Country")
+    fig = px.pie(sales_by_country, values='Amount', names='Country', hole=0.3)
+    st.plotly_chart(fig)
+
+# Q4 Group by salesperson
+top_salespersons = filtered_df.groupby('Sales Person')['Amount'].sum().sort_values(ascending=False).head(3)
+
+# chart 4
+top_salespersons = filtered_df.groupby('Sales Person')['Amount'].sum().nlargest(3).reset_index()
+with col8:
+    st.subheader("Top 3 Salespersons")
+    fig = px.bar(top_salespersons, x='Sales Person', y='Amount', color='Sales Person')
+    st.plotly_chart(fig)
